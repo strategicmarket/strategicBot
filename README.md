5dbe2c25d98d6f8147bec38cea9d994cad6a6c96
adamtre9115 <treadam9115@gmail.com>
2018-06-09 09:10:37 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SM Bot
+
+SM Bot will eventually be ‘activated’ by a microservice and will be able to interact with the customer about the capability of the Strategic Machine platform.
---
+++ index.js
@@ -0,0 +1,9 @@
+////////////////////////////////////////////////////////////////////
+////////////             Strategic Machines           /////////////
+///////////         Strategic Bot    c2018          //////////////
+/////////////////////////////////////////////////////////////////
+const Botkit =      require('botkit')
+const Controller =  Botkit.slackbot({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+})
