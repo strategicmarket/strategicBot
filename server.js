e4a229238d11f80843a95fe8564aaa3a1e2a123b
adamtre9115 <treadam9115@gmail.com>
2018-06-19 21:49:36 -0400

    update server and move app to routes... test for apimachine connect

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
-const express =     require("express")
-const bodyParser =  require("body-parser");
 const PORT =        process.env.PORT || 3500
 const index =       require("./bot/index")
-const app =         express()
+const app = require("./routes/app.js")
 
-// general api handling
-const main =        express.Router()
-require("./routes/main")(main);
-
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-// call main.js for display 
-app.get('/', main)
-
-// call slack bot for use 
-app.use(index);
 
 app.listen(PORT, function(){
     console.log(`server is now running on ${PORT}`)
